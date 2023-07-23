b660a3555af39e9a35de440e098d530360ebfba6
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-07-23 21:34:27 +0300

    added the category slider with styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 
 // components
 import Header from "./components/header/Header";
+import Category from "./components/category slider/Category";
 
 function App() {
   return (
     <>
       <Header />
+      <Category />
     </>
   );
 }
---
+++ src/components/category slider/Category.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+// styling
+import "./category.scss";
+
+const Category = () => {
+  return (
+    <section className="category">
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+      <article className="item">sport</article>
+    </section>
+  );
+};
+
+export default Category;
