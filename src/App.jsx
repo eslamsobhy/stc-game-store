66528ad7abc5ff95a5bcf4a20798ebb0dc32cc9e
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-07-23 18:42:48 +0300

    cleared the boilerplate code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="app">App Component</div>
+    </>
+  );
+}
+
+export default App;
