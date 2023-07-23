fc5a6292e268d04804233aa53e85a78d8f0bfce0
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-07-23 20:58:35 +0300

    added styling for the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 
+// styling
+import "./header.scss";
+
 // icons
 import { AiOutlineSearch, AiOutlineMore } from "react-icons/ai";
 
 const Header = () => {
   return (
     <header>
-      <section className="header">
+      <section className="section header">
         <div className="logo">
           <img src="/assets/logo.png" alt="" />
           <div className="title">Game Store</div>
         </div>
         <div className="actions">
-          <AiOutlineSearch />
-          <AiOutlineMore />
+          <AiOutlineSearch color="var(--primary-color)" size={25} />
+          <AiOutlineMore color="var(--primary-color)" size={25} />
         </div>
       </section>
     </header>
